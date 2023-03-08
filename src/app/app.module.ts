a8aceed9576624a64ba596f34858d82ab037744a
HelenMayer <you@example.com>
2023-03-08 13:04:03 +0200

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { CustomersModule } from './customers/customers.module';
 import { AppComponent }  from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { OrdersModule } from './orders/orders.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports:      [ BrowserModule, CustomersModule, OrdersModule, SharedModule, CoreModule, AppRoutingModule ],
+  imports:      [ BrowserModule, CustomersModule, OrdersModule, SharedModule, CoreModule, AppRoutingModule, NgbModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
